b0aafbb0735f311fda01a7881234e52025254e31
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-24 13:13:09 -0400

    Adding challenge 2

--- JavaScriptCardio/session2/app.js
+++ JavaScriptCardio/session2/app.js
@@ -1,4 +1,4 @@
-// CHALLENGE 1: LONGEST WORD COMMENT
+// ====== CHALLENGE 1: LONGEST WORD ====== COMMENT
 // Return the longest word of a string. If more than one longest word, put it into an array
 //ex. longestEotf('Hello, my name is Brad') === 'hello'
 //ex. longestWord('Hello there, my name is Brad') === ['hello', 'there']
@@ -21,12 +21,45 @@ const longestWord = (sen) => {
     console.log(longestWordArr);
   }
 };
-longestWord("Hello there, my name is Brad");
+// longestWord("Hello there, my name is Brad");
 
-// CHALLENGE 2: ARRAY CHUNKING COMMENT
-const chunkArray = (arr, len) => {};
+// ====== CHALLENGE 2: ARRAY CHUNKING ====== COMMENT
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1,2,3,4,5,6,7], 3) === [[1,2,3], [4,5,6], [7]]
+// ex. chunkArray([1,2,3,4,5,6,7], 2) === [[1,2], [3,4], [5,6], [7]]
+const chunkArray = (arr, len) => {
+  // SOLUTION 1: WHILE LOOP
+  //   // Initialize chunked array
+  //   const chunkedArray = [];
+  //   // Set index
+  //   let i = 0;
+  //   // Loop while index is less than the array length
+  //   while (i < arr.length) {
+  //     // Slice out from the index to the index + the chunk length and push on to the chunked array
+  //     chunkedArray.push(arr.slice(i, i + len));
+  //     // Increment by chunk length
+  //     i += len;
+  //   }
+  //   console.log(chunkedArray);
+  // SOLUTION 2: FOREACH HIGH ORDER
+  //init
+  const chunkedArr = [];
+  //loop through arr
+  arr.forEach((val) => {
+    //Get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+    //Check if last and if last length is equal to the chunk len
+    if (!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+  console.log(chunkedArr);
+};
+// chunkArray([1, 2, 3, 4, 5, 6, 7], 2);
 
-// CHALLENGE 3: FLATTERN ARRAY COMMENT
+// ====== CHALLENGE 3: FLATTEN ARRAY ====== COMMENT
 const flattenArray = (arrays) => {};
 
-// CHALLENGE 4: ANAGRAM COMMENT
+// ====== CHALLENGE 4: ANAGRAM ====== COMMENT
---
+++ JavaScriptCardio/session2/breakdown.md
@@ -0,0 +1,7 @@
+# JavaScript Cardio Session 2
+
+With each challenge, there are stars (\*\*\*) next to the section separators that indicate what the creator would do as the best option in an interview.
+
+## Methods Glossary
+
+- **split()** - Converts a string into an array
