513d422f701fcfc45be2feafe1e57026c3d8b945
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-27 12:45:50 -0400

    Added sum all primes solution

--- JavaScriptCardio/session3/app.js
+++ JavaScriptCardio/session3/app.js
@@ -11,4 +11,32 @@ const addAll = (...numbers) => {
   //   console.log(total);
   console.log(numbers.reduce((acc, cur) => acc + cur));
 };
-addAll(2, 5, 6, 7, 5);
+// addAll(2, 5, 6, 7, 5);
+
+// ====== CHALLENGE 2: SUM ALL PRIMES ======
+// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex, sumAllPrimes(10) == 17
+const sumAllPrimes = (num) => {
+  let total = 0;
+  const checkForPrime = (i) => {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (let i = 2; i <= num; i++) {
+    if (checkForPrime(i)) {
+      total += i;
+    }
+  }
+  console.log(total);
+};
+// sumAllPrimes(10);
+
+// ====== CHALLENGE 3: SEEK & DESTROY
+// Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
+// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+const seekAndDestroy = () => {};
