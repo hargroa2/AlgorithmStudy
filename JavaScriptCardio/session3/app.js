2b9ef8a1ce5ffdb5d3cdcf9080901216e3175aa8
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-27 12:33:23 -0400

    Add all numbers solution

--- JavaScriptCardio/session3/app.js
+++ JavaScriptCardio/session3/app.js
@@ -1,1 +1,14 @@
-
+// ====== CHALLENGE 1: ADD ALL NUMBERS ======
+// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
+//ex. addAll(2, 5, 6, 7) === 20
+const addAll = (...numbers) => {
+  //rest operator gives all of the parameters in an array
+  //... SOLUTIONS: rest and reduce/forEach
+  //   let total = 0;
+  //   numbers.forEach((num) => {
+  //     total += num;
+  //   });
+  //   console.log(total);
+  console.log(numbers.reduce((acc, cur) => acc + cur));
+};
+addAll(2, 5, 6, 7, 5);
