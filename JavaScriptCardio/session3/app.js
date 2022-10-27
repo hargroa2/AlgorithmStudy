8885103c51bd280e8fc33642a64431607a71611f
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-27 12:59:54 -0400

    Added seek and destroy solution

--- JavaScriptCardio/session3/app.js
+++ JavaScriptCardio/session3/app.js
@@ -1,4 +1,4 @@
-// ====== CHALLENGE 1: ADD ALL NUMBERS ======
+// ====== CHALLENGE 1: ADD ALL NUMBERS ====== COMMENT
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 //ex. addAll(2, 5, 6, 7) === 20
 const addAll = (...numbers) => {
@@ -13,7 +13,7 @@ const addAll = (...numbers) => {
 };
 // addAll(2, 5, 6, 7, 5);
 
-// ====== CHALLENGE 2: SUM ALL PRIMES ======
+// ====== CHALLENGE 2: SUM ALL PRIMES ====== COMMENT
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex, sumAllPrimes(10) == 17
 const sumAllPrimes = (num) => {
@@ -36,7 +36,19 @@ const sumAllPrimes = (num) => {
 };
 // sumAllPrimes(10);
 
-// ====== CHALLENGE 3: SEEK & DESTROY
-// Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
+// ====== CHALLENGE 3: SEEK & DESTROY ====== COMMENT
+// Remove from the array whatever is in the following arguments. Return the leftover values in an array
+// destroys the 2s and the 6s vvv
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
-const seekAndDestroy = () => {};
+const seekAndDestroy = (arr, ...rest) => {
+  //solution: ...rest, filter and includes
+  console.log(arr.filter((val) => !rest.includes(val)));
+};
+// seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6);
+
+// ====== CHALLENGE 4: SORT BY HEIGHT ====== COMMENT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex. a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+const sortByHeight = () => {};
+sortByHeight();
