9879aaa0073fa0384a209d1e11e791c8c459209c
Amanda Hargrove <amandahargrove98@gmail.com>
2022-10-27 13:09:28 -0400

    Add sort by height solution

--- JavaScriptCardio/session3/app.js
+++ JavaScriptCardio/session3/app.js
@@ -50,5 +50,20 @@ const seekAndDestroy = (arr, ...rest) => {
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
 // ex. a = [-1, 150, 190, 170, -1, -1, 160, 180]
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
-const sortByHeight = () => {};
-sortByHeight();
+const sortByHeight = (a) => {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => {
+    if (val === -1) {
+      arr1.push(i);
+    } else {
+      arr2.push(val);
+    }
+  });
+  const sortArr = arr2.sort((a, b) => a - b); //sorts it lowest to highest. Highest to lowest would be b - a
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1)); //putting the -1 into its correct position
+  console.log(sortArr);
+};
+const a = [-1, 150, 190, 170, -1, -1, 160, 180];
+sortByHeight(a);
