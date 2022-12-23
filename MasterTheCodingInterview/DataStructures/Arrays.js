6b204551b460394d8aad355008ad1f7603635c0d
Amanda Hargrove <amandahargrove98@gmail.com>
2022-12-23 13:36:37 -0500

    Adding array data structure creation using classes

--- MasterTheCodingInterview/DataStructures/Arrays.js
+++ MasterTheCodingInterview/DataStructures/Arrays.js
@@ -66,4 +66,50 @@ class Wizard extends Player {
 
 const wizard1 = new Wizard("Shelly", "Healer"); // Make an instance of Wizard
 const wizard2 = new Wizard("Sean", "Dark Magic");
-wizard1.play();
+// wizard1.play();
+
+// ======== CREATING AN ARRAY DATA STRUCTURE USING CLASSES ========
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1]; //Shifting item to the left by 1
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
+
+const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("you");
+newArray.push("are");
+newArray.push("nice");
+// console.log(newArray);
+newArray.delete(1);
+// console.log(newArray);
