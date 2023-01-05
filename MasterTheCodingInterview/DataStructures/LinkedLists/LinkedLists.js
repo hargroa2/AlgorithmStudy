83afcd03eb4bf48cb6292929ff93503d07cd7a1b
Amanda Hargrove <amandahargrove98@gmail.com>
2023-01-05 16:25:57 -0500

    Added insert method to linkedlist class

--- MasterTheCodingInterview/DataStructures/LinkedLists/LinkedLists.js
+++ MasterTheCodingInterview/DataStructures/LinkedLists/LinkedLists.js
@@ -81,13 +81,41 @@ class LinkedList {
     return array;
   }
 
-  insert(index, value) {}
+  insert(index, value) {
+    //check parameters
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    console.log(this.printList);
+    return this.printList;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    console.log(currentNode);
+    return currentNode;
+  }
+
+  remove(index) {}
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
+myLinkedList.insert(2, 99);
 myLinkedList.printList();
-// myLinkedList.insert(2, 99);
-console.log(myLinkedList);
+// console.log(myLinkedList);
