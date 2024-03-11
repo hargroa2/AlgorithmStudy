b9d7941d6d180804fa6b7fcd40554de213764dc6
Amanda Hargrove <amandahargrove98@gmail.com>
2024-03-11 14:49:27 -0400

    tested problem solving algorithm

--- JavaScriptMasterClass/BigOExamples.js
+++ JavaScriptMasterClass/BigOExamples.js
@@ -58,7 +58,7 @@ let logAtMost5 = (n) => {
   }
 };
 
-console.log(logAtMost5(6)); // O(1), at max 5 operations
+// console.log(logAtMost5(6)); // O(1), at max 5 operations
 
 function subtotals(array) {
   var subtotalArray = Array(array.length);
@@ -91,4 +91,30 @@ let double = (arr) => {
   }
   return newArr;
 };
-console.log(double([1, 2, 3, 4]));
+// console.log(double([1, 2, 3, 4]));
+
+// PROBLEM SOLVING EXAMPLE
+let charCount = (str) => {
+  let obj = {};
+  for (let char of str) {
+    if (isAlphaNumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  }
+  return obj;
+};
+
+let isAlphaNumeric = (char) => {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+};
+
+// console.log(charCount("Hello WORLD hi!!!"));
