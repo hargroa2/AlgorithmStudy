513bdcd08e2fd48285f9691dde5e4bd58e4dcbcc
Amanda Hargrove <amandahargrove98@gmail.com>
2024-03-11 18:33:33 -0400

    Update ProblemPatterns.js

--- JavaScriptMasterClass/ProblemSolvingPatterns/ProblemPatterns.js
+++ JavaScriptMasterClass/ProblemSolvingPatterns/ProblemPatterns.js
@@ -83,3 +83,9 @@ countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
 countUniqueValues([]) // 0
 countUniqueValues([-2, -1, -1, 0, 1]) // 4
 */
+
+let countUniqueValues = (arr) => {
+  //
+};
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
