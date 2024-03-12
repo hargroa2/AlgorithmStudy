63e92d5f15d5c1c1390395a9f0594463e6653015
Amanda Hargrove <amandahargrove98@gmail.com>
2024-03-12 15:55:04 -0400

    unique values algorithm

--- JavaScriptMasterClass/ProblemSolvingPatterns/ProblemPatterns.js
+++ JavaScriptMasterClass/ProblemSolvingPatterns/ProblemPatterns.js
@@ -85,13 +85,18 @@ countUniqueValues([-2, -1, -1, 0, 1]) // 4
 */
 
 let countUniqueValues = (arr) => {
-  // Use 2 pointers in order to loop through the array, and if the number has not shown up in the array yet, add 1 to the count. If it has already shown up, do not add to the count and keep going
-  // let counter = 0;
-  // for (let i = 0; i < arr.length; i++) {
-  //     for (let j = 0; j < arr.length; i++) {
-  //         if (arr[i] === arr[j]) {}
-  //     }
-  // }
+  // compare the two pointers and compare how many times they're not equal. Could make a variable for this. If the numbers are the same, move j forward but leave i where it is. If the numbers are different, move i forward and put the counter into the index of that array.
+  if (arr.length === 0) {
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
 };
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([1, 2, 2, 5, 7, 7, 99]));
